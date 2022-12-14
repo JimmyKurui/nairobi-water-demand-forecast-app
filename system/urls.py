c158c4194a5d467056443f8bbb0b2bc8e1e7b59c
JimmyKurui <jimmy.chepkurui@strathmore.edu>
2022-12-15 01:33:12 +0300

    App Views 3/4

--- system/urls.py
+++ system/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.homepage, name="idnex"),
+    path('', views.homepage, name="index"),
+    path('/dashboard', views.dashboard, name="dashboard"),
+    path('/contribute', views.contribute, name="contribute"),
+    path('/support', views.support, name="support"),
+    path('/about', views.about, name="about"),
 ]
